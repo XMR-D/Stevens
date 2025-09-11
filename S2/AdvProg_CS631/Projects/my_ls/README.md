97ed653a45da19023022b5cd4becdc1fee8ef220
XMR <guillaume.wantiez@epita.fr>
2025-09-11 11:44:43 -0400

    Refactoring

--- S2/AdvProg_CS631/Projects/my_ls/README.md
+++
@@ -1,1 +0,0 @@
-my_ls - reimplementation of ls builtin
--- S2/AdvProg_CS631/Projects/my_ls/src/include/tokenize.h
+++
@@ -1,9 +0,0 @@
-#ifndef TOKENIZE_H
-#define TOKENIZE_H
-
-#include "targ-parser.h"
-
-int 
-Tokenize(int argc, char * input[], TargList * head, TargList * tail);
-
-#endif  /* !_TOKENIZE_H */
