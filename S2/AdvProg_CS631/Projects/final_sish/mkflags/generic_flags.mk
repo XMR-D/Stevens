49717c953b5024123074d4c3fb96b88d718d25a3
XMR-D <88503168+XMR-D@users.noreply.github.com>
2025-10-21 20:48:31 -0400

    Delete S2 directory

--- S2/AdvProg_CS631/Projects/final_sish/mkflags/generic_flags.mk
+++
@@ -1,5 +0,0 @@
-CC= gcc
-CFLAGS=-Wall -Wextra -Werror -pedantic -std=c99 -O0 -I src/include
-DEBUGFLAGS= -g -fsanitize=leak -O0 -Wall -Wextra -Werror -pedantic -std=c99 -I src/include
-LDFLAGS=-lm
-SRC_DIR= src
